fa387bfb38f22afdb34099d6df1c5979f48dd6f9
baegteun <baegteun@gmail.com>
2022-06-20 16:01:29 +0900

    :sparkles: :: AuthDetails

---
+++ src/main/java/com/msg/after_school/global/security/auth/AuthDetailsService.java
@@ -0,0 +1,22 @@
+package com.msg.after_school.global.security.auth;
+
+import com.msg.after_school.domain.user.exception.UserNotFoundException;
+import com.msg.after_school.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class AuthDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findUserByEmail(username)
+                .map(AuthDetails::new)
+                .orElseThrow(() -> UserNotFoundException.EXCEPTION);
+    }
+}
