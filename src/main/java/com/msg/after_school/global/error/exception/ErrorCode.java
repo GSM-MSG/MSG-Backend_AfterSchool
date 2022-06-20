ea2549de578392ad82470bd9c6ee36995eff8867
baegteun <baegteun@gmail.com>
2022-06-20 09:34:19 +0900

    :sparkles: :: GlobalExceptionHandler

---
+++ src/main/java/com/msg/after_school/global/error/exception/ErrorCode.java
@@ -0,0 +1,15 @@
+package com.msg.after_school.global.error.exception;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
+public enum ErrorCode {
+    ;
+
+    private final int status;
+    private final String message;
+}
