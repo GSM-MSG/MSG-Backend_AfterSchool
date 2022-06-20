0577733be3a6aa7e9d9a30cc97e05ab2952b6c12
baegteun <baegteun@gmail.com>
2022-06-20 15:59:01 +0900

    :sparkles: :: JwtPreperties

---
+++ src/main/java/com/msg/after_school/global/security/properties/JwtProperties.java
@@ -0,0 +1,16 @@
+package com.msg.after_school.global.security.properties;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+
+@Getter
+@ConstructorBinding
+@AllArgsConstructor
+@ConfigurationProperties(prefix = "jwt")
+public class JwtProperties {
+    private final String accessSecret;
+    private final String refreshSecret;
+}
