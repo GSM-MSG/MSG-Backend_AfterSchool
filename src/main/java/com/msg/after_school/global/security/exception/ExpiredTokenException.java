3e81c6725787d1c87c15cc707c635cececfde412
Huuuunee <s21024@gsm.hs.kr>
2022-06-20 22:25:44 +0900

    Service구현중

--- src/main/java/com/msg/after_school/global/security/exception/ExpiredTokenException.java
+++ src/main/java/com/msg/after_school/global/security/exception/ExpiredTokenException.java
@@ -6,7 +6,7 @@ import com.msg.after_school.global.error.exception.ErrorCode;
 public class ExpiredTokenException extends AfterSchoolException {
     public static final AfterSchoolException EXCEPTION = new ExpiredTokenException();
 
-    private  ExpiredTokenException() {
+    private ExpiredTokenException() {
         super(ErrorCode.EXPIRED_TOKEN);
     }
 
--- src/main/java/com/msg/after_school/service/impl/AfterSchoolServiceImpl.java
+++ src/main/java/com/msg/after_school/service/impl/AfterSchoolServiceImpl.java
@@ -1,9 +1,12 @@
 package com.msg.after_school.service.impl;
 
+import com.msg.after_school.domain.AfterSchool;
+import com.msg.after_school.domain.DayOfWeek;
 import com.msg.after_school.dto.AfterSchoolDto;
 import com.msg.after_school.dto.SearchConditionDto;
 import com.msg.after_school.repository.AfterSchoolRepository;
 import com.msg.after_school.service.AfterSchoolService;
+import com.msg.after_school.type.SeasonType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,7 +21,11 @@ public class AfterSchoolServiceImpl implements AfterSchoolService {
 
     @Override
     @Transactional
-    public List<AfterSchoolDto> findAfterSchoolListBySearchCondition(SearchConditionDto dto) {
+    public List<AfterSchoolDto> findAfterSchoolListBySearchCondition(SearchConditionDto searchConditionDto) {
+        Integer grade = searchConditionDto.getGrade();
+        SeasonType season = searchConditionDto.getSeason();
+        DayOfWeek dayOfWeek = searchConditionDto.getWeek();
+        List<AfterSchool> Data = afterSchoolRepository.findAllByGradeAndAndDayOfWeekAndSeason(grade,season,dayOfWeek);
         return null;
     }
 }
