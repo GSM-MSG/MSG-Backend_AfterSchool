bdd81e7a0ce9d7280d61ed7d032c6c4c67a0951b
baegteun <baegteun@gmail.com>
2022-06-28 07:01:14 +0900

    :sparkles: :: Logout Service

--- src/main/java/com/msg/after_school/domain/auth/service/LogoutService.java
+++ src/main/java/com/msg/after_school/domain/auth/service/LogoutService.java
@@ -1,2 +1,17 @@
-package com.msg.after_school.domain.auth.service;public class LogoutService {
+package com.msg.after_school.domain.auth.service;
+
+import com.msg.after_school.domain.user.data.entity.User;
+import com.msg.after_school.global.user.facade.UserFacade;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogoutService {
+    private final UserFacade userFacade;
+
+    public void execute() {
+        User user = userFacade.getCurrentUser();
+        user.updateRefreshToken(null);
+    }
 }
