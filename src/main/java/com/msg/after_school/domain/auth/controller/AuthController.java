9f1c36e553ce69a1aa0e8186a4a0f58b46b104a3
baegteun <baegteun@gmail.com>
2022-06-27 23:45:52 +0900

    :sparkles: :: Refresh routing

--- src/main/java/com/msg/after_school/domain/auth/controller/AuthController.java
+++ src/main/java/com/msg/after_school/domain/auth/controller/AuthController.java
@@ -3,6 +3,7 @@ package com.msg.after_school.domain.auth.controller;
 import com.msg.after_school.domain.auth.data.dto.TokenDto;
 import com.msg.after_school.domain.auth.service.LoginService;
 import com.msg.after_school.domain.auth.service.RedirectService;
+import com.msg.after_school.domain.auth.service.RefreshService;
 import com.msg.after_school.domain.auth.utils.CookieUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -21,6 +22,7 @@ public class AuthController {
     private final CookieUtil cookieUtil;
     private final LoginService loginService;
     private final RedirectService redirectService;
+    private final RefreshService refreshService;
 
     @GetMapping("/login")
     @ResponseBody
@@ -36,6 +38,15 @@ public class AuthController {
         response.addCookie(accessCookie);
         response.addCookie(refreshCookie);
 
-        return "FRONTURL";
+        return "redirect://FRONTURL/";
+    }
+
+    @PatchMapping("/refresh")
+    public void refresh(HttpServletResponse res) {
+        TokenDto token = refreshService.execute();
+        Cookie accessCookie = cookieUtil.createCookie("accessToken", token.getAccessToken(), token.getAccessExp());
+        Cookie refreshCookie = cookieUtil.createCookie("refreshToken", token.getRefreshToken(), token.getRefreshExp());
+        res.addCookie(accessCookie);
+        res.addCookie(refreshCookie);
     }
 }
