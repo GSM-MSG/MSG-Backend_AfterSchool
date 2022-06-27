364944dcc0e51dca8473d7dcc4e2924861b1ea39
baegteun <baegteun@gmail.com>
2022-06-27 20:33:28 +0900

    :sparkles: :: google-properties

---
+++ src/main/java/com/msg/after_school/global/security/properties/GoogleProperties.java
@@ -0,0 +1,19 @@
+package com.msg.after_school.global.security.properties;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+
+@Getter
+@ConstructorBinding
+@AllArgsConstructor
+@ConfigurationProperties(prefix = "google")
+public class GoogleProperties {
+    private final String authUrl;
+    private final String loginUrl;
+    private final String redirectUrl;
+    private final String clientId;
+    private final String clientSecret;
+    private final String scope;
+}
