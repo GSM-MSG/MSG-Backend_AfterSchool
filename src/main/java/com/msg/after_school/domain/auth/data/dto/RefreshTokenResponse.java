8fd85cb64bdf3f08bb8ef5867b28f6d220d9b005
baegteun <baegteun@gmail.com>
2022-07-04 08:49:34 +0900

    :sparkles: :: getter..?

--- src/main/java/com/msg/after_school/domain/auth/data/dto/RefreshTokenResponse.java
+++ src/main/java/com/msg/after_school/domain/auth/data/dto/RefreshTokenResponse.java
@@ -1,9 +1,11 @@
 package com.msg.after_school.domain.auth.data.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 import javax.servlet.http.Cookie;
 
+@Getter
 @AllArgsConstructor
 public class RefreshTokenResponse {
     Cookie accessToken;
