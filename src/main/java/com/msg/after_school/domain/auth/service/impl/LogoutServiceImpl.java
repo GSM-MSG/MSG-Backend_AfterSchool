f9f5e521158324a4d3e7481006cd32393ae295e2
baegteun <baegteun@gmail.com>
2022-07-04 08:37:11 +0900

    :sparkles: :: in transactional

--- src/main/java/com/msg/after_school/domain/auth/service/impl/LogoutServiceImpl.java
+++ src/main/java/com/msg/after_school/domain/auth/service/impl/LogoutServiceImpl.java
@@ -6,8 +6,11 @@ import com.msg.after_school.global.user.dao.UserDao;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class LogoutServiceImpl implements LogoutService {
     private final UserDao userDao;
 
