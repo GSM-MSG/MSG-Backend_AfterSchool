74f3f33d6f232eac5d7de03d666ee5ddbf598fe2
baegteun <baegteun@gmail.com>
2022-07-04 10:57:38 +0900

    :fire: :: unused import

--- src/main/java/com/msg/after_school/domain/after_school/data/dto/response/AfterSchoolListResponseDto.java
+++ src/main/java/com/msg/after_school/domain/after_school/data/dto/response/AfterSchoolListResponseDto.java
@@ -2,10 +2,8 @@ package com.msg.after_school.domain.after_school.data.dto.response;
 
 import com.msg.after_school.domain.after_school.data.entity.DayOfWeek;
 import com.msg.after_school.domain.after_school.data.entity.Grade;
-import com.msg.after_school.domain.after_school.data.type.SeasonType;
 import lombok.*;
 
-import javax.persistence.*;
 import java.util.List;
 
 @Getter
